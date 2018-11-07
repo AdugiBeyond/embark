475ea965345ef82a1508b9b16f3fda40474d4432
Anthony Laibe <anthony@laibe.cc>
2018-11-07 14:21:17 +0000

    WIP

--- embark-ui/src/components/FileExplorer.js
+++ embark-ui/src/components/FileExplorer.js
@@ -1,8 +1,9 @@
 import {AppSwitch} from '@coreui/react';
-import {Label} from 'reactstrap';
+import {Label, UncontrolledTooltip} from 'reactstrap';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Treebeard, decorators} from 'react-treebeard';
+import FontAwesome from 'react-fontawesome';
 import classNames from 'classnames';
 import {DARK_THEME} from '../constants';
 
@@ -77,66 +78,129 @@ const style = (theme) => ({
   }
 });
 
-
-const Header = ({style, node}) => {
-  let icon;
-
-  if (!node.children) {
-    const extension = node.path.split('.').pop();
-    switch(extension) {
-      case 'html':
-        icon = 'text-danger fa fa-html5';
-        break;
-      case 'css':
-        icon = 'text-warning fa fa-css3';
-        break;
-      case 'js':
-      case 'jsx':
-        icon = 'text-primary icon js-icon';
-        break;
-      case 'json':
-        icon = 'text-success icon hjson-icon';
-        break;
-      case 'sol':
-        icon = 'text-warning icon solidity-icon';
-        break;
-      default:
-        icon = 'fa fa-file-o';
-    }
-  } else {
-    switch(node.name) {
-      case 'dist':
-        icon = 'text-danger icon easybuild-icon';
-        break;
-      case 'config':
-        icon = 'text-warning fa fa-cogs';
-        break;
-      case 'contracts':
-        icon = 'text-success fa fa-file-text';
-        break;
-      case 'app':
-        icon = 'text-primary fa fa-code';
-        break;
-      case 'test':
-        icon = 'icon test-dir-icon';
-        break;
-      case 'node_modules':
-        icon = 'fa fa-folder-o';
-        break;
-      default:
-        icon = 'fa fa-folder';
-    }
-
+class Container extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {active: null};
   }
 
-  return (
-    <div className="mb-1" style={style.base}>
-      <div style={style.title}>
-        <i className={classNames('mr-1', icon)} />
-        {node.name}
+  activateNode() {
+    this.setState({active : true});
+  }
+  
+  deactivateNode() {
+    this.setState({active : false});
+  }
+
+  renderAction() {
+    return (
+      <span className="float-right mr-2">
+        {this.props.node.children &&
+          <React.Fragment>
+            <a id="add-file"><FontAwesome name="plus" className="text-success mr-2" /></a>
+            <a id="add-folder"><FontAwesome name="folder-open" className="text-success mr-2" /></a>
+            <UncontrolledTooltip placement="bottom" target="add-file">
+              Add File
+            </UncontrolledTooltip>
+            <UncontrolledTooltip placement="bottom" target="add-folder">
+              Add Folder
+            </UncontrolledTooltip>
+          </React.Fragment>
+        }
+        <a id="delete"><FontAwesome name="trash" className="text-danger" /></a>
+        <UncontrolledTooltip placement="bottom" target="delete">
+          Delete
+        </UncontrolledTooltip>
+      </span>
+    )
+  }
+
+  render() {
+    return (
+      <div style={this.props.style.container}
+           onClick={this.props.onClick}
+           onMouseEnter={() => this.activateNode()} 
+           onMouseLeave={() => this.deactivateNode()}>
+        <this.props.decorators.Toggle style={this.props.style.toggle}/>
+        <this.props.decorators.Header node={this.props.node} style={this.props.style.header}/>
+        {this.state.active && this.renderAction()}
       </div>
-    </div>
-  );
+    )
+  }
+}
+
+Container.propTypes = {
+  style: PropTypes.object,
+  node: PropTypes.object
+};
+
+class Header extends React.Component {
+  resolveIcon() {
+    let icon;
+    let {node} = this.props;
+
+    if (!node.children) {
+      const extension = node.path.split('.').pop();
+      switch(extension) {
+        case 'html':
+          icon = 'text-danger fa fa-html5';
+          break;
+        case 'css':
+          icon = 'text-warning fa fa-css3';
+          break;
+        case 'js':
+        case 'jsx':
+          icon = 'text-primary icon js-icon';
+          break;
+        case 'json':
+          icon = 'text-success icon hjson-icon';
+          break;
+        case 'sol':
+          icon = 'text-warning icon solidity-icon';
+          break;
+        default:
+          icon = 'fa fa-file-o';
+      }
+    } else {
+      switch(node.name) {
+        case 'dist':
+          icon = 'text-danger icon easybuild-icon';
+          break;
+        case 'config':
+          icon = 'text-warning fa fa-cogs';
+          break;
+        case 'contracts':
+          icon = 'text-success fa fa-file-text';
+          break;
+        case 'app':
+          icon = 'text-primary fa fa-code';
+          break;
+        case 'test':
+          icon = 'icon test-dir-icon';
+          break;
+        case 'node_modules':
+          icon = 'fa fa-folder-o';
+          break;
+        default:
+          icon = 'fa fa-folder';
+      }
+    }
+
+    return icon;
+  }
+
+  render() {
+    let {node, style} = this.props;
+    return (
+      <div className="mb-1 d-inline-block" 
+           style={style.base}>
+        <div style={style.title}>
+          <i className={classNames('mr-1', this.resolveIcon())} />
+          {node.name}
+        </div>
+      </div>
+    );
+  }
 };
 
 Header.propTypes = {
@@ -145,6 +209,7 @@ Header.propTypes = {
 };
 
 decorators.Header = Header;
+decorators.Container = Container;
 
 class FileExplorer extends React.Component {
   constructor(props) {
